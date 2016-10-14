19c45e3bd13c5b6975c8170acf460a89d71ee855
Jessica Stokes <hello@jessicastokes.net>
2016-10-14 19:21:58 +1100

    Switch to actual Unicode characters in code

--- src/detect/fitzpatrick.js
+++ src/detect/fitzpatrick.js
@@ -3,6 +3,6 @@ import { prepareCanvasContext, getCharacterWidth } from '../canvas';
 export default function detectFitzpatrick(context) {
   context = prepareCanvasContext(context);
 
-  return getCharacterWidth('\u{1F478}' /* Princess */, context)
-    === getCharacterWidth('\u{1F478}\u{1F3FE}' /* Princess, Type-5 */, context);
+  return getCharacterWidth('👸' /* Princess */, context)
+    === getCharacterWidth('👸🏿' /* Princess, Type-5 */, context);
 }
--- src/detect/genders.js
+++ src/detect/genders.js
@@ -3,6 +3,6 @@ import { prepareCanvasContext, getCharacterWidth } from '../canvas';
 export default function detectGenders(context) {
   context = prepareCanvasContext(context);
 
-  return getCharacterWidth('\u{1F575}' /* Sleuth or Spy */, context)
-    === getCharacterWidth('\u{1F575}\u{FE0F}\u{200D}\u{2640}\u{FE0F}' /* Female Sleuth */, context);
+  return getCharacterWidth('🕵' /* Sleuth or Spy */, context)
+    === getCharacterWidth('🕵🏻‍♀️' /* Female Sleuth */, context);
 }
